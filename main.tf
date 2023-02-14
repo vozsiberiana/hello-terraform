a227e87193f5000562caa9d2ea92a26f3cd2ce5c
Anna Knyazkina <ann.knyazkina@gmail.com>
2023-02-14 15:51:31 +0100

    Created main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0b752bf1df193a6c4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
