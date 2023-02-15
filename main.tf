9721c473c74c134a6cc394367d4014a2429477a3
Anna Knyazkina <ann.knyazkina@gmail.com>
2023-02-15 08:34:15 +0100

    Edited main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0b752bf1df193a6c4"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-0e00978364e148d44"
-  subnet_id = "subnet-0a777c7272fde2cf6"
+  ami                    = "ami-0b752bf1df193a6c4"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0e00978364e148d44"]
+  subnet_id              = "subnet-0a777c7272fde2cf6"
+  key_name               = "key-amazon"
 
   tags = {
     Name = "TerraformInstance"
