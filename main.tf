5b14ee08c27cba2460b894bf313610e17632bbf3
Anna Knyazkina <ann.knyazkina@gmail.com>
2023-02-15 10:46:01 +0100

    Added script to main.tf

--- main.tf
+++ main.tf
@@ -24,5 +24,19 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
     APP  = "hello-2048"
   }
-}
+  user_data = <<EOH
+#!/bin/sh
+amazon-linux-extras install -y docker
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+pip3 install docker-compose
+mkdir /home/ec2-user/hello-2048
+cd /home/ec2-user/hello-2048
+wget https://raw.githubusercontent.com/vozsiberiana/hello-2048/main/docker-compose.yml
+chown ec2-user docker-compose.yml
+docker-compose pull
+docker-compose up -d
+EOH
 
+}
